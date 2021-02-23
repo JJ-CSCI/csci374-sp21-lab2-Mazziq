1bf446b58e8f5a08818b52610d44ea8786fbaa85
Mazziq <snoziznasty@gmail.com>
2021-02-23 02:54:49 +0000

    i

--- main.fs
+++ main.fs
@@ -1,14 +1,25 @@
-module Assignment
+
+u8y
+
+
+kjk,jyhku=mzx77odule Assignment
 
 type AMPM = AM | PM
 
 // This function checks if an hour value `h` is not in [1,12] range
 let areHoursInvalid h =
-    false
+     if h < 1 || h > 12
+     true 
+     else
+     h = 0
 
 // This function checks if a minute value `m` is not in [0,59] range
 let areMinutesInvalid m =
-    false
+  if m < 1 || m >59
+  j/
+  else 
+  m = 0
+  
 
 // This function creates a valid time tuple
 //      use above functions: areHoursInvalid & areMinutesInvalid
